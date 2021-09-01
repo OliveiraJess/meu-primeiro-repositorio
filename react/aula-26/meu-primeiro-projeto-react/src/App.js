9c4b79d81288294127a3c2a033c0c147b782a91e
Jessica de Oliveira <jessica-lm-@hotmail.com>
2021-09-01 19:27:09 -0300

    react

--- react/aula-26/meu-primeiro-projeto-react/src/App.js
+++ react/aula-26/meu-primeiro-projeto-react/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Identidade(props) {
+  return <h1>Meu nome é {props.name} e eu tenho {props.idade} anos</h1>;
+  
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Identidade name="Jessica" idade="24" />
     </div>
+    
   );
 }
-
 export default App;
