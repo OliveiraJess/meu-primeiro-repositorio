33f9b140615dd991cd43f1a2bbee8458cde087de
Jessica de Oliveira <jessica-lm-@hotmail.com>
2021-09-12 21:28:29 -0300

    react-lista-tarefa-fazer

--- react/aula-29/semana-2/gerenciador-de-tarefas/src/components/tarefa/Tarefa.jsx
+++ react/aula-29/semana-2/gerenciador-de-tarefas/src/components/tarefa/Tarefa.jsx
@@ -1,14 +1,18 @@
 import { AppVisualizar } from "../../pages/visualizar/visualizar"
 import "./tarefa.css"
+import { useHistory } from "react-router-dom";
+
+
 export default function Tarefa({tarefa, deletar}) {
+    const history = useHistory();
 
     return(
         <div className="tarefa">
             <h1>{tarefa.titulo}</h1>
             <div className="botoes">
-                <button className="editar"  onClick={() => alert("cliquei aqui editar")}>E</button>
+                <button className="editar"  onClick={() => history.push("/editar")}>E</button>
                 <button className="deletar" onClick={() => deletar(tarefa.id)}>X</button>
-                <button className="vizualizar" onClick={() => AppVisualizar()}>V</button>
+                <button className="vizualizar" onClick={() => history.push("/visualizar")}>V</button>
             </div>
         </div>
     )
--- react/aula-29/semana-2/gerenciador-de-tarefas/src/pages/visualizar/visualizar.jsx
+++ react/aula-29/semana-2/gerenciador-de-tarefas/src/pages/visualizar/visualizar.jsx
@@ -4,20 +4,25 @@ import Tarefa from "../../components/tarefa/Tarefa";
 import { useState } from "react";
 import Botao from "../../components/botao/Botao";
 import TarefaEditar from "../../components/tarefa-editar/TarefaEditar";
+import { useHistory } from "react-router-dom";
+
+
 
 export function AppVisualizar() {
+    const history = useHistory();
+
   return (
     <main>
       <div className="container">
 
-        <ExternalCard title="Lista De Tarefas - vizualizar">
+        <ExternalCard title="Lista De Tarefas - Visualizar">
           <div className="row space-evenly">
-            <Botao>Voltar</Botao>
+            <button onClick={() => history.push("/")}>Voltar</button>
             <span className="flex2"></span>
           </div>
 
           <TarefaEditar title="Minha primeira tarefa" readOnly="true">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum, saepe quidem animi illo, accusamus consequuntur, voluptatum ipsa voluptatem quo repudiandae nihil veritatis? Ipsa fuga dolorem est labore itaque dolores corporis.
+          vizualizar
           </TarefaEditar>
         </ExternalCard>
 
